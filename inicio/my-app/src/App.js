452081adf1b850f576e31a32bb0b03946d60f86d
Ileana Aguilar <ileanaap9@gmail.com>
2022-07-22 16:00:06 -0700

    Cambios clase 2 Byron jerarquia componentes

--- inicio/my-app/src/App.js
+++ inicio/my-app/src/App.js
@@ -1,20 +1,19 @@
 import "./App.css";
 import "./Components/Base/AppBar/AppBar";
 import AppBar from "./Components/Base/AppBar/AppBar";
-import TimeBar from "./Components/Base/AppBar/TimeBar";
+//import TimeBar from "./Components/Base/AppBar/TimeBar";
 
 function App() {
+
+  const alerta = () => {
+    alert("Esta es una alerta");
+  }
   return (
     <div className="App">
       <AppBar
-        //aqui le pasamos los props
-        title="MI PRIMERA APP"
-        subtitle="Ya funciona!"
-        time = {<TimeBar />}
-      />
-      <AppBar
-        title="MI SEGUNDA APP"
-        subtitle="Ya funciona tambien!"
+      title="MI PRIMER APP"
+      subtitle="Si funciona!"
+      myAlert={alerta}
       />
     </div>
   );
--- inicio/my-app/src/Components/Base/AppBar/AppBar.jsx
+++ inicio/my-app/src/Components/Base/AppBar/AppBar.jsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './AppBar.css';
-import TimeBar from './TimeBar';
+import React from "react";
+import PropTypes from "prop-types";
+import Boton from "../Boton/Boton";
+
+//import TimeBar from "./TimeBar";
+//react imports
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Dropdown from "react-bootstrap/Dropdown";
+//local css imports
+import "./AppBar.css";
+
 
 AppBar.propTypes = {
-    //propiedades que se le deben pasar
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-    hour: PropTypes.number
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  myAlert: PropTypes.func,
 };
 
 //props lista por default que tienen los componentes
 //los tres puntos le dice que jale todos los props que tienen los componentes por default (los va a tener disponibles pero no guardados)
 //con los {} se dice que se le pasa un objeto
-function AppBar({title = "TITULO", subtitle = "Subtitulo", time, ...props}) {
-    return (
-        <div className='appbar'>
-            <h1>{title}</h1>
-            <h4>{subtitle}</h4>
-            <h3>{time}</h3> {/*muestra nuestro componente de la hora*/}
-        </div>
-    );
+function AppBar({
+  title = "TITULO",
+  subtitle = "Subtitulo",
+  myAlert,
+  ...props
+}) {
+  const miFuncionPropia = () => {
+    console.log("Esto ejecutando mi funcion propia");
+    myAlert();
+  };
+
+  return (
+    <Navbar bg="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home" className="titulo-app-bar">
+          {title}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link id="home" href="#home">
+              Home
+            </Nav.Link>
+            <Nav.Link id="link" href="#" onClick={myAlert}>
+              Link
+            </Nav.Link>
+            <Boton title="Mi Boton NIETO" action={miFuncionPropia} />
+
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Dropdown Button
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default AppBar;
