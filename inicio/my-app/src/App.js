cc173822c980d6dda5244f20f3c48ca6ac9211c4
Ileana Aguilar <ileanaap9@gmail.com>
2022-08-04 19:47:09 -0700

    React Router Practice

--- inicio/my-app/src/App.js
+++ inicio/my-app/src/App.js
@@ -3,6 +3,79 @@ import "./Components/Base/AppBar/AppBar";
 import AppBar from "./Components/Base/AppBar/AppBar";
 //import TimeBar from "./Components/Base/AppBar/TimeBar";
 
+import { BrowserRouter as Router, Switch, Route, Link, NavLink, Redirect } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        {/* routes go here, as children */}
+        <Route exact path="/" render={() => <Home />} /> {/* propiedad exact se asegura de que el enrutado coincida exactamente con la ruta '/' */}
+        <Route path="/about" component={About} />
+        <Route path="/know"> <Know /> </Route> {/* you can use them without render o component */}
+        <Route path="*" component={NotFound} /> {/* el asterisco marca una ruta 404, no existente */}
+
+        <PrivateRoute path="/hidden" component={Hidden} /> {/* aqui tenemos un componente de ruta privada */}
+      </Switch>
+    </Router>
+  );
+}
+
+function PrivateRoute({ component: Component, ...rest}) {
+  //useAuth es un hook personalizado para obtener el estado de autenticación del usuario actual
+  const isAuth = useAuth();
+
+  return (
+    <Route 
+      {...rest}
+      render={(props) =>
+        isAuth ? <Component {...props} /> : <Redirect to="/" />
+      }
+    />
+  );
+}
+
+function Hidden() {
+  return <>hidden</>;
+}
+
+function NotFound() {
+  return <>Ha llegado a una página que no existe</>
+}
+
+function Navbar() {
+  //visible on every page
+  return (
+    <nav>
+      {/* ejemplo de NavLink component */}
+      <NavLink activeStyle={{fontWeight: "bold", color: "red"}} to="/">
+        Home
+      </NavLink>
+      <NavLink activeClassName="active" to="/about">
+        About
+      </NavLink>
+
+      {/* ejemplo de Link component */}
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </nav>
+  )
+}
+
+function Know() {
+  return <>Know</>;
+}
+
+function Home() {
+  return <>Home</>;
+}
+
+function About() {
+  return <>about</>;
+}
+
+/*
 function App() {
 
   const alerta = () => {
@@ -19,4 +92,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
